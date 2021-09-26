c6747cbcc068fc3517394ba93e9725a6f4319b6c
Naufal Alfadhil <nafaarts.com@gmail.com>
2021-09-26 18:28:36 +0700

    trying add sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { routes } from "./src/route";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  sitemap: {
-    baseURL: "https://n.nafaarts.com",
-    routes,
-  },
 });
